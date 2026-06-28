b031d3aa3f27a583676bdd9263c3b50e5e85f6a8
Rodrigo Leal <rodrigocleal@gmail.com>
2026-06-28 18:54:12 +0100

    chore: add backup rules to gitignore, track docker, startup script, and migration plugin

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Install Docker and Docker Compose
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl enable docker
+systemctl start docker
+# Add user to docker group
+usermod -aG docker ubuntu
+usermod -aG docker $USER
